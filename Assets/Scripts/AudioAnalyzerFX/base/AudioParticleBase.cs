f7be3d8ebe7a6893b490644a4f36cdef79ef9e2f
darksatellite <code@iedlabs.net>
2016-11-25 19:39:18 -0600

    got distracted from working on features and began working on dissecting audio analysis... switching to windowing branch

--- Assets/Scripts/AudioAnalyzerFX/base/AudioParticleBase.cs
+++ Assets/Scripts/AudioAnalyzerFX/base/AudioParticleBase.cs
@@ -6,21 +6,25 @@ public class AudioParticleBase : AudioFXBase
 {
 
     protected ParticleSystem ps;
+    protected ParticleSystem.EmissionModule em;
     protected ParticleSystem.Particle[] particles;
-    
 
     protected virtual void Start()
     {
         ps = GetComponent<ParticleSystem>();
+        em = ps.emission;
+        particles = new ParticleSystem.Particle[ps.maxParticles];
     }
 
-
-    protected ParticleSystem.Particle[] ProcessParticles()
+    protected virtual void Update()
+    {
+        ProcessParticles();
+    }
+    
+    protected virtual void ProcessParticles()
     {
         int pCount = ps.GetParticles(particles);
-
         // do something to each particle, or emit particles, etc. 
-        return particles;
+        ps.SetParticles(particles, pCount);
     }
-
 }
