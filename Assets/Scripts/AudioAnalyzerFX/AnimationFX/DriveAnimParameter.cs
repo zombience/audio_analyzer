83f4abeedb0fd7d1f93b0a6818b60a3ad999c1c2
darksatellite <code@iedlabs.net>
2016-12-12 15:14:03 -0600

    implemented animation fx & tuned to particular audio bands

---
+++ Assets/Scripts/AudioAnalyzerFX/AnimationFX/DriveAnimParameter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveAnimParameter : AudioAnimationBase
+{
+
+    [SerializeField]
+    protected string targetParam = "animSpeed";
+    
+        
+    protected override void Update()
+    {
+        anim.SetFloat(targetParam, bandValue);
+        
+    }
+}
---
+++ Assets/Scripts/AudioAnalyzerFX/base/AudioAnimationBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AudioAnimationBase : AudioFXBase
+{
+
+    protected Animator anim;
+    protected AnimatorStateInfo currentState, previousState;
+    
+	protected virtual void Start ()
+    {
+        anim = GetComponent<Animator>();
+        previousState = currentState = anim.GetCurrentAnimatorStateInfo(0);
+	}
+
+    protected virtual void Update()
+    {
+
+    }
+}
