212824ebf498f90f1e3b18917d8ca4cfac9d85bb
zombience <code@iedlabs.net>
2017-09-01 18:27:34 -0500

    updated InspectorBase to distinguish between MonoBehaviours and ScriptableObjects

--- Assets/Scripts/AudioAnalyzer/Core/Editor/InspectorBase.cs
+++ Assets/Scripts/AudioAnalyzer/Core/Editor/InspectorBase.cs
@@ -6,58 +6,23 @@ using UnityEditor;
 
 namespace AudioAnalyzer.EditorUtilities
 {
-	public class ScriptableObjectInspectorBase<T> : Editor where T : ScriptableObject
-	{
-		protected T obj;
-		protected GUIStyle style;
-		protected float inc		= .1f;
-		protected Color bg		= Color.cyan;
 
-		protected virtual void OnEnable()
-		{
-			obj = target as T;
-			style = new GUIStyle();
-			GUIUtilities.ColorUpdater = UpdateColors;
-		}
+	public class InspectorBase : UnityEditor.Editor
 
-		public override void OnInspectorGUI()
-		{
-			base.OnInspectorGUI();
-			bg = Color.cyan;
-		}
-
-
-		protected void Space(float pixels)
-		{
-			GUILayout.Space(pixels);
-		}
-
-		void UpdateColors()
-		{
-			GUI.backgroundColor = bg;
-			bg.g -= inc;
-			bg.r += inc;
-			if (bg.g <= 0 || bg.g > 1f) inc *= -1f;
-		}
-	}
-	
-	public class InspectorBase<T> : Editor where T : MonoBehaviour
 	{
 
-		protected T obj;
-		protected float inc = .1f;
 		protected GUIStyle style;
+		protected float	inc = .1f;
 		protected Color bg = Color.cyan;
 		protected bool showDefaultInspector = true;
 
-		protected Context 
-			play	= Context.PLAYING,
-			editor	= Context.NOTPLAYING, 
-			either	= Context.EITHER;
+		protected Context play = Context.PLAYING;
+		protected Context editor = Context.NOTPLAYING;
+		protected Context either = Context.EITHER;
 
 		protected virtual void OnEnable()
 		{
-			obj = target as T;
+
 			style = new GUIStyle();
 			GUIUtilities.ColorUpdater = UpdateColors;
 		}
@@ -71,10 +36,9 @@ namespace AudioAnalyzer.EditorUtilities
 			bg = Color.cyan;
 		}
 
-
-		protected void Space(float pixels)
+		protected void Space(float height)
 		{
-			GUILayout.Space(pixels);
+			GUILayout.Space(height);
 		}
 
 		void UpdateColors()
@@ -83,6 +47,34 @@ namespace AudioAnalyzer.EditorUtilities
 			bg.g -= inc;
 			bg.r += inc;
 			if (bg.g <= 0 || bg.g > 1f) inc *= -1f;
+		}
+	}
+
+	public class InspectorScriptableObjectBase<T> : InspectorBase where T : ScriptableObject
+	{
+		protected T obj;
+
+		protected override void OnEnable()
+		{
+			obj = target as T;
+			base.OnEnable();
+		}
+	}
+
+	/// <summary>
+	/// because this is used to generate custom inspectors and I like creating custom inspectors
+	/// inside the same script as monobehaviours, this class cannot be inside an Editor Folder
+	/// otherwise they cannot be referenced from Monobehaviour scripts
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class InspectorMonoBase<T> : InspectorBase where T : MonoBehaviour
+	{
+		protected T obj;
+
+		protected override void OnEnable()
+		{
+			obj = target as T;
+			base.OnEnable();
 		}
 	}
 
@@ -95,6 +87,7 @@ namespace AudioAnalyzer.EditorUtilities
 
 	public static partial class GUIUtilities
 	{
+
 		static public System.Action ColorUpdater { private get; set; }
 
 		static public bool IsCurrent(this Context c)
@@ -114,17 +107,31 @@ namespace AudioAnalyzer.EditorUtilities
 
 		static public void Action(this Context c, System.Action action)
 		{
-			if (!c.IsCurrent()) return;
-			if (action != null) action();
+			if (c.IsCurrent() && action != null) action();
 		}
+
 
 		static public void Button(this Context c, string label, System.Action action = null)
 		{
-			if (!c.IsCurrent())			return;
-			if(ColorUpdater != null)	ColorUpdater();
+			if (!c.IsCurrent()) return;
+			if (ColorUpdater != null) ColorUpdater();
 
-			if (GUILayout.Button(label) && action != null)  action();
+			if (GUILayout.Button(label))
+				if (action != null) action();
 		}
+
+		static public void Button(this Context c, string label, Color color, float height = 40, System.Action action = null)
+		{
+			if (!c.IsCurrent()) return;
+			Color bg = GUI.backgroundColor;
+			GUI.backgroundColor = color;
+
+			if (GUILayout.Button(label, GUILayout.Height(height)))
+				if (action != null) action();
+
+			GUI.backgroundColor = bg;
+		}
+
 
 		static public void Label(this Context c, string label)
 		{
