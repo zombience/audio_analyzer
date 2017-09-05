ef716da5d15cf51dd4e505573c4668b4b0678c4b
zombience <code@iedlabs.net>
2017-09-05 18:48:40 -0500

    renamed particle growth --- sourcetree keeps missing files. what gives?

---
+++ Assets/Scripts/AudioAnalyzer/AAComponents/ParticleFX/ParticleGrowth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+namespace AudioAnalyzer
+{
+	public class ParticleGrowth : ParticleAFXBase
+	{
+		[SerializeField]
+		protected float growthSpeed = .75f;
+		[SerializeField, Range(0.05f, 0.95f)]
+		protected float threshold = .3f;
+
+		[SerializeField]
+		protected Color targetColor = Color.cyan;
+
+		protected override void ProcessParticles()
+		{
+			if (band.bandValue > threshold)
+			{
+				int pCount = ps.GetParticles(particles);
+				for (int i = 0; i < pCount; i++)
+				{
+					if (particles[i].remainingLifetime > psMain.startLifetime.constant * .9f)
+					{
+						particles[i].startSize += growthSpeed;
+						particles[i].startColor = targetColor * band.bandValue;
+					}
+				}
+				ps.SetParticles(particles, pCount);
+			}
+		}
+	}
+}
