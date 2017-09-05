5376c257ec8b42752dafb9c03c67d416b4c7113b
zombience <code@iedlabs.net>
2017-09-05 18:47:38 -0500

    modified some editor utils

--- Assets/Scripts/AudioAnalyzer/Core/Editor/InspectorBase.cs
+++ Assets/Scripts/AudioAnalyzer/Core/Editor/InspectorBase.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 namespace AudioAnalyzer.EditorUtilities
 {
 
-	public class InspectorBase : Editor
+	abstract public class InspectorBase : Editor
 	{
 		protected GUIStyle	style;
 		protected float		inc	= .1f;
@@ -18,9 +18,8 @@ namespace AudioAnalyzer.EditorUtilities
 
 		protected virtual void OnEnable()
 		{
-
 			style = new GUIStyle();
-			GUIUtilities.ColorUpdater = UpdateColors;
+			Utilities.ColorUpdater = UpdateColors;
 		}
 
 		public override void OnInspectorGUI()
@@ -46,7 +45,7 @@ namespace AudioAnalyzer.EditorUtilities
 		}
 	}
 
-	public class InspectorScriptableObjectBase<T> : InspectorBase where T : ScriptableObject
+	abstract public class InspectorScriptableObjectBase<T> : InspectorBase where T : ScriptableObject
 	{
 		protected T obj;
 
@@ -63,7 +62,7 @@ namespace AudioAnalyzer.EditorUtilities
 	/// otherwise they cannot be referenced from Monobehaviour scripts
 	/// </summary>
 	/// <typeparam name="T"></typeparam>
-	public class InspectorMonoBase<T> : InspectorBase where T : MonoBehaviour
+	abstract public class InspectorMonoBase<T> : InspectorBase where T : MonoBehaviour
 	{
 		protected T obj;
 
@@ -79,82 +78,5 @@ namespace AudioAnalyzer.EditorUtilities
 		PLAYING,
 		NOTPLAYING,
 		EITHER
-	}
-
-	public static partial class GUIUtilities
-	{
-
-		static public System.Action ColorUpdater { private get; set; }
-
-		static public bool IsCurrent(this Context c)
-		{
-			switch (c)
-			{
-				case Context.PLAYING:
-					return Application.isPlaying;
-				case Context.NOTPLAYING:
-					return !Application.isPlaying;
-				case Context.EITHER:
-					return true;
-				default:
-					return false;
-			}
-		}
-
-		static public void Action(this Context c, System.Action action)
-		{
-			if (c.IsCurrent() && action != null) action();
-		}
-
-
-		static public void Button(this Context c, string label, System.Action action = null)
-		{
-			if (!c.IsCurrent()) return;
-			if (ColorUpdater != null) ColorUpdater();
-
-			if (GUILayout.Button(label))
-				if (action != null) action();
-		}
-
-		static public void Button(this Context c, string label, Color color, float height = 40, System.Action action = null)
-		{
-			if (!c.IsCurrent()) return;
-			Color bg = GUI.backgroundColor;
-			GUI.backgroundColor = color;
-
-			if (GUILayout.Button(label, GUILayout.Height(height)))
-				if (action != null) action();
-
-			GUI.backgroundColor = bg;
-		}
-
-
-		static public void Label(this Context c, string label)
-		{
-			if (!c.IsCurrent()) return;
-			GUILayout.Label(label);
-		}
-
-		static public Texture2D BGTexture(this Color color)
-		{
-			var texture = new Texture2D(1, 1)
-			{
-				hideFlags = HideFlags.DontSave
-			};
-			texture.SetPixel(0, 0, color);
-			texture.Apply();
-			return texture;
-		}
-
-		static public void SectionLabel(this GUIStyle style, string label, int height, Color color)
-		{
-			GUILayout.Space(10);
-			style.normal.background = color.BGTexture();
-			GUILayout.BeginHorizontal(style, GUILayout.Height(height));
-			GUILayout.Label(label);
-			GUILayout.Space(height);
-			GUILayout.EndHorizontal();
-			GUILayout.Space(10);
-		}
 	}
 }
--- Assets/Scripts/Utilities/Editor/EditorUtilities.cs
+++ Assets/Scripts/Utilities/Editor/EditorUtilities.cs
@@ -45,9 +45,98 @@ namespace AudioAnalyzer.EditorUtilities
 		}
 	}
 
-	public static class Utilities
+	public static partial class Utilities
 	{
-		
+
+		static public System.Action ColorUpdater { private get; set; }
+
+		static public bool IsCurrent(this Context c)
+		{
+			switch (c)
+			{
+				case Context.PLAYING:
+					return Application.isPlaying;
+				case Context.NOTPLAYING:
+					return !Application.isPlaying;
+				case Context.EITHER:
+					return true;
+				default:
+					return false;
+			}
+		}
+
+		static public void Action(this Context c, System.Action action)
+		{
+			if (c.IsCurrent() && action != null) action();
+		}
+
+
+		static public void Button(this Context c, string label, System.Action action = null)
+		{
+			if (!c.IsCurrent()) return;
+			if (ColorUpdater != null) ColorUpdater();
+
+			if (GUILayout.Button(label))
+				if (action != null) action();
+		}
+
+		static public void Button(this Context c, string label, Color color, float height = 40, System.Action action = null)
+		{
+			if (!c.IsCurrent()) return;
+			Color bg = GUI.backgroundColor;
+			GUI.backgroundColor = color;
+
+			if (GUILayout.Button(label, GUILayout.Height(height)))
+				if (action != null) action();
+
+			GUI.backgroundColor = bg;
+		}
+
+
+		static public void Label(this Context c, string label)
+		{
+			if (!c.IsCurrent()) return;
+			GUILayout.Label(label);
+		}
+
+		static public Texture2D BGTexture(this Color color)
+		{
+			var texture = new Texture2D(1, 1)
+			{
+				hideFlags = HideFlags.DontSave
+			};
+			texture.SetPixel(0, 0, color);
+			texture.Apply();
+			return texture;
+		}
+
+		static public void SectionLabel(this Context c, GUIStyle style, string label, Color color, int height, int width = 0)
+		{
+			if (!c.IsCurrent()) return;
+			style.SectionLabel(label, color, height, width);
+		}
+
+		static public void SectionLabel(this GUIStyle style, string label, Color color, int height, int width = -1)
+		{
+			GUILayout.Space(5);
+			style.normal.background = color.BGTexture();
+			style.alignment = TextAnchor.UpperCenter;
+
+			if (width < 0)
+			{
+				GUILayout.BeginHorizontal(style, GUILayout.Height(height));
+			}
+			else
+			{
+				GUILayout.BeginHorizontal(style, new GUILayoutOption[]
+				{ GUILayout.Height(height), GUILayout.Width(width)});
+			}
+
+			GUILayout.Label(label);
+			GUILayout.Space(height);
+			GUILayout.EndHorizontal();
+			GUILayout.Space(5);
+		}
 	}
 
 }
