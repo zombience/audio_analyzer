16198e7b6fd3002c015b186d864507dc6bc89060
zombience <code@iedlabs.net>
2017-09-07 14:44:04 -0500

    INIT dev branch: this branch will be for working on features and fixes. adding PostProcessing for possible integration, and an IK Target script for possible future use

---
+++ Assets/Scripts/AudioAnalyzer/AAComponents/AnimationFX/FollowIK.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+
+namespace AudioAnalyzer
+{
+	public class FollowIK : AnimationAFXBase
+	{
+
+		[SerializeField]
+		bool multiplyByMasterBand;
+
+		[SerializeField]
+		TargetHelper[] ikTargets;
+
+		public float overrideamount = 1f;
+
+		void OnAnimatorIK()
+		{
+			for (int i = 0; i < ikTargets.Length; i++)
+			{
+				TargetHelper t = ikTargets[i];
+
+				float value = multiplyByMasterBand ? t.Value * band.bandValue : t.Value;
+
+				if(t.isLookTarget)
+				{
+					anim.SetLookAtWeight(overrideamount);
+					anim.SetLookAtPosition(t.Position);
+				}
+				else
+				{
+					anim.SetIKPositionWeight(t.bodyPart, overrideamount);
+					anim.SetIKRotationWeight(t.bodyPart, overrideamount);
+					anim.SetIKPosition(t.bodyPart, t.Position);
+					anim.SetIKRotation(t.bodyPart, t.Rotation);
+				}
+			}
+		}
+
+		[System.Serializable]
+		class TargetHelper
+		{
+			public BandValueNormalized band;
+
+			public Transform	target;
+			public AvatarIKGoal bodyPart;
+			public bool			isLookTarget;
+
+			public float		Value		{ get { return band.bandValue; } }
+			public Vector3		Position	{ get { return target.position; } }
+			public Quaternion	Rotation	{ get { return target.rotation; } }
+		}
+	}
+
+
+}
